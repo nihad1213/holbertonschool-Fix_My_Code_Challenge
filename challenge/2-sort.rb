755a1c5b6d63f5654d578bb56aa78c5c469c7f39
nihad1213 <nihad.nemetli@gmail.com>
2024-09-13 17:34:59 +0400

    a

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,31 +1,31 @@
 ###
 #
-#  Sort integer arguments (ascending)
+#  Sort integer arguments (ascending) 
 #
 ###
 
 result = []
 ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-    # convert to integer
-    i_arg = arg.to_i
-    # insert result at the right position
-    result << i_arg
-end
+  # skip if not integer
+  next if arg !~ /^-?[0-9]+$/
 
-n = result.length
-loop do
-    swapped = false
+  # convert to integer
+  i_arg = arg.to_i
 
-    (n-1).times do |i|
-        if result[i] < result[i+1]
-            result[i], result[i+1] = result[i+1], result[i]
-            swapped = true
-        end
+  # insert result at the right position
+  is_inserted = false
+  i = 0
+  l = result.size
+  while !is_inserted && i < l do
+    if result[i] >= i_arg
+      result.insert(i, i_arg)
+      is_inserted = true
+      break
+    else
+      i += 1
     end
-
-    break if not swapped
+  end
+  result << i_arg unless is_inserted
 end
 
-puts result
+puts result.join(" ")
